a0a9400343b792fd7ef1f9888a48d07733ea2bae
Jon Friskics <jonfriskics@gmail.com>
2018-09-27 11:27:45 -0400

    module 2 task 3

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -4,6 +4,8 @@ namespace StarChart.Models
     public class CelestialObject
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+
         public CelestialObject()
         {
         }
