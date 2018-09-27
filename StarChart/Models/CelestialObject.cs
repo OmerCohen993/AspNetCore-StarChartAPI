c6f03d02928c23b78e221d4782812fc7ecb4dee7
Jon Friskics <jonfriskics@gmail.com>
2018-09-27 11:29:10 -0400

    module 2 task 4

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace StarChart.Models
 {
     public class CelestialObject
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        public int? OrbitedObjectId { get; set; }
 
         public CelestialObject()
         {
